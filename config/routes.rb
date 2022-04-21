81f85819211acfd03d7e15b0e0fbec2ec82566c3
Moshe <moisherosenzweig@gmail.com>
2022-04-21 11:24:51 -0400

    sessions_working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :applications
   resources :jobs
   resources :companies
-  resources :seekers
+  resources :seekers, only:[:create, :destroy]
   resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/me", to: "seekers#show"
