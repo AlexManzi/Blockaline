c0279c7b047ee0b52fba88d03a17b25a8613884c
Alex Manzi <alex.manzi22@gmail.com>
2022-04-21 13:42:40 -0400

    push

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,5 +6,12 @@ class FavoritesController < ApplicationController
     rescue ActiveRecord::RecordNotFound
         render json:{errors:"Episode Not Found"}, status:404
     end
+
+    def create
+        favorite = Favorites.create!(params.permit(:seeker_id, :job_id))
+        render json: favorite, status: :ok
+    rescue ActiveRecord::RecordInvalid
+        render json: {errors:"fav not added"}, status: 404
+    end
     
 end
